f4bf1c5ef3988acea98ed78c2288f5f3eac357cd
best-lucky <bestlucky1030@gmail.com>
2021-09-27 23:39:24 +0300

    update event card and list components.

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -2,9 +2,9 @@
   <div class="event-card">
     <div class="card">
       <div class="card-content">
-        <h2 class="is-size-4 has-text-weight-bold">Event Name</h2>
-        <small class="event-date">Event Date</small>
-        <span>Event location</span>
+        <h2 class="is-size-4 has-text-weight-bold">{{event.name}}</h2>
+        <small class="event-date">{{event.date}}</small>
+        <span>{{event.location}}</span>
       </div>
     </div>
   </div>
@@ -13,7 +13,10 @@
 <script>
 
 export default {
-  name: 'EventCard'
+  name: 'EventCard',
+  props: [
+    'event'
+  ]
 }
 </script>
 
