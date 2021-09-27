e9c81813947ef8b9a9d2e63d13ae331b8814fe79
best-lucky <bestlucky1030@gmail.com>
2021-09-28 01:54:12 +0300

    update eventsingle page.

--- src/views/EventSingle.vue
+++ src/views/EventSingle.vue
@@ -5,11 +5,13 @@
       <div class="hero-body">
         <div class="container">
           <h1 class="title">
-            Event name
+            {{event.name}}
           </h1>
 
           <h2 class="subtitle">
-            Event date
+            <strong>Date: </strong> {{event.date}}
+            <br>
+            <strong>Time: </strong> {{event.time}}
           </h2>
         </div>
       </div>
@@ -17,12 +19,12 @@
 
     <section class="event-content">
       <div class="container">
-        <p class="is-size-4 description">Event description</p>
-        <p class="is-size-4">Location:</p>
-        <p class="is-size-4">Category:</p>
+        <p class="is-size-4 description">{{event.description}}</p>
+        <p class="is-size-4"><strong>Location: </strong> {{event.location}}</p>
+        <p class="is-size-4"><strong>Category: </strong> {{event.category}}</p>
         <div class="event-images columns is-multiline has-text-centered">
-          <div class="column is-one-third">
-            IMAGE PLACEHOLDER
+          <div v-for="image in event.images" :key="image.id" class="column is-one-third">
+            <img :src="image" :alt="event.name" />
           </div>
         </div>
       </div>
@@ -34,7 +36,47 @@
 <script>
 
   export default {
-    name: 'EventSingle'
+    name: 'EventSingle',
+    data () {
+      return {
+        events: [
+          {
+            id: 1,
+            name: 'Charity Ball',
+            category: 'Fundraising',
+            description: 'Spend an elegant night of dinner and dancing with us as we raise money for our new rescue farm.',
+            featuredImage: 'https://placekitten.com/500/500',
+            images: [
+              'https://placekitten.com/500/500',
+              'https://placekitten.com/500/500',
+              'https://placekitten.com/500/500',
+            ],
+            location: '1234 Fancy Ave',
+            date: '09-25-2021',
+            time: '11:30'
+          },
+          {
+            id: 2,
+            name: 'Rescue Center Goods Drive',
+            category: 'Adoptions',
+            description: 'Come to our donation drive to help us replenish our stock of pet food, toys, bedding, etc. We will have live bands, games, food trucks, and much more.',
+            featuredImage: 'https://placekitten.com/500/500',
+            images: [
+              'https://placekitten.com/500/500'
+            ],
+            location: '1234 Dog Alley',
+            date: '11-21-2020',
+            time: '12:00'
+          }
+        ],
+        event: {}
+      }
+    },
+    created() {
+      const ID = Number(this.$route.params.id);
+      let event = this.events.find(event => event.id === ID);
+      this.event = event;
+    }
   }
 
 </script>
